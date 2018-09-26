c35c9bd9b7e6125f95211d78be5bef2bbcb9407e
hleebarton97 <hleebarton97@gmail.com>
2018-09-26 17:43:22 -0500

    heroku deployment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Render static react build page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 require('./routes/getRoutes')(app);
 
